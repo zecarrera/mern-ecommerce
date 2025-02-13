2250875422589cc582c0692d8e254f3cd7bbd59a
Jose Carrera <jose.carrera@amido.com>
2025-02-13 16:58:45 +0000

    attempts to upgrade mongoose to latest

--- server/utils/db.js
+++ server/utils/db.js
@@ -8,19 +8,10 @@ const { database } = keys;
 const setupDB = async () => {
   try {
     // Connect to MongoDB
-    mongoose.set('useCreateIndex', true);
-    mongoose
-      .connect(database.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-      })
-      .then(() =>
-        console.log(`${chalk.green('✓')} ${chalk.blue('MongoDB Connected!')}`)
-      )
-      .catch(err => console.log(err));
+    await mongoose.connect(database.url);
+    console.log(`${chalk.green('✓')} ${chalk.blue('MongoDB Connected!')}`);
   } catch (error) {
-    return null;
+    console.error(error);
   }
 };
 
